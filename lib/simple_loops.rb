562a271141e31f43f8c8ecfc948eeae5566c5492
Anthony Polito <anthonyalexanderpolito@gmail.com>
2020-07-26 17:28:48 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,7 +1,7 @@
 # Write your methods here
-def loop_message_n_times(message, limit)
+def loop_message_five_times(message)
   count = 0
-  while count < limit do
+  while count < 5 do
     puts message
     count += 1
   end
